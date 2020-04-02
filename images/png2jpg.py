69180be48778283f3443234804f356cbf9798fb5
ZQPei <dfzspzq@163.com>
2020-04-02 18:09:28 +0800

    update

---
+++ images/png2jpg.py
@@ -0,0 +1,21 @@
+import os
+import glob
+import PIL.Image as Image
+
+
+def convert_jpg(fn):
+    fn_new = os.path.splitext(fn)[0] + ".jpg"
+    img = Image.open(fn)
+    img = img.convert("RGB")
+    img.save(fn_new)
+
+
+def main():
+    root = "./"
+    png_flist = glob.glob(root+"*.png")
+    for fn in png_flist:
+        convert_jpg(fn)
+
+
+if __name__ == "__main__":
+    main()
